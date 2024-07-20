4980693b7fff11e5115899657a7b19354b326ee7
David <avilagarciajesusdavid41@gmail.com>
2024-07-19 22:02:14 -0600

    cambio env

--- src/components/WebSocketComponent.js
+++ src/components/WebSocketComponent.js
@@ -20,12 +20,16 @@ const WebSocketComponent = () => {
       console.log('WebSocket connection closed: ', event);
     };
 
+    // Cleanup function to close the WebSocket connection
     return () => {
-      socket.close();
+      if (socket.readyState === WebSocket.OPEN) { // WebSocket.OPEN equals 1
+        socket.close();
+        console.log('WebSocket connection closed due to component unmount');
+      }
     };
   }, []);
 
-  return null
+  return null 
 };
 
 export default WebSocketComponent;
