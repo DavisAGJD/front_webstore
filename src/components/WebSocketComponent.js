6bf4277c755d860d4ab70369c78326c3060ec096
David <avilagarciajesusdavid41@gmail.com>
2024-07-19 20:17:03 -0600

    cambios websocket

--- src/components/WebSocketComponent.js
+++ src/components/WebSocketComponent.js
@@ -1,31 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useCallback } from 'react';
 
 const WebSocketComponent = () => {
-  useEffect(() => {
-    const socket = new WebSocket(process.env.REACT_APP_WS_URL);
+    const socketRef = useRef(null);
 
-    socket.onopen = () => {
-      console.log('WebSocket connection established');
-    };
+    const connectWebSocket = useCallback(() => {
+        socketRef.current = new WebSocket('wss://backend-webstore.onrender.com/ws');
 
-    socket.onmessage = (event) => {
-      console.log('Message from server ', event.data);
-    };
+        socketRef.current.onopen = () => {
+            console.log('WebSocket connection established');
+        };
 
-    socket.onerror = (error) => {
-      console.error('WebSocket error: ', error);
-    };
+        socketRef.current.onmessage = (event) => {
+            console.log('Message from server ', event.data);
+        };
 
-    socket.onclose = (event) => {
-      console.log('WebSocket connection closed: ', event);
-    };
+        socketRef.current.onerror = (error) => {
+            console.error('WebSocket error: ', error);
+        };
 
-    return () => {
-      socket.close();
-    };
-  }, []);
+        socketRef.current.onclose = (event) => {
+            console.log('WebSocket connection closed: ', event);
+            // Retry connection logic
+            if (!event.wasClean) {
+                console.log('Connection closed unexpectedly. Retrying...');
+                setTimeout(() => {
+                    // Recursively try to reconnect
+                    connectWebSocket();
+                }, 1000); // retry after 1 second
+            }
+        };
+    }, []); // Empty dependency array ensures this is created once
 
-  return null;
+    useEffect(() => {
+        connectWebSocket();
+
+        // Clean up on component unmount
+        return () => {
+            if (socketRef.current) {
+                socketRef.current.close();
+            }
+        };
+    }, [connectWebSocket]); // Add connectWebSocket to the dependency array
+
+    return null; // No need to render anything
 };
 
 export default WebSocketComponent;
